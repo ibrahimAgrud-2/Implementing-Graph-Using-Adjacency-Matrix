d5f3f05931ee4a2667ea7d3d773fcbc9548c5626
ibrahim <ibrahimagrud7@gmail.com>
2026-09-02 11:36:37 +0300

    Created directed/undirected Matix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -19,24 +19,37 @@ namespace BinaryTreeImplementation
             List<string> Vertices;
             public Graph(List<string> vertices,GraphDirectionType enGraphDirectionType)
             {
+                //eleman sayısı kadar dizi oluştur. Mesela 3 elemanlı ise 3x3 array
                 adjacencyMatrix=new int[vertices.Count,vertices.Count];
-                this.Vertices=new List<string>(vertices);
+                this.Vertices=vertices;
             }
 
             public void AddEdge(string node1,string node2,int connectionValue)
             {
+
+                
                 if(Vertices.Contains(node1)&&Vertices.Contains(node2))
                 {
-                     adjacencyMatrix[Vertices.IndexOf(node1),Vertices.IndexOf(node2)]=connectionValue;
+                    if(enGraphDirectionType==GraphDirectionType.enDirected)
+                    {
+                         adjacencyMatrix[Vertices.IndexOf(node1),Vertices.IndexOf(node2)]=connectionValue;
+                    }
+                    else
+                    {
+                         adjacencyMatrix[Vertices.IndexOf(node1),Vertices.IndexOf(node2)]=connectionValue;
+                         adjacencyMatrix[Vertices.IndexOf(node2),Vertices.IndexOf(node1)]=connectionValue;
+                    }
                 }
             } 
 
 
 
-            public void DisplayMatix()
+            public void DisplayMatix(string title)
             {
+           
                for (int i = 0; i < adjacencyMatrix.GetLength(0); i++)
                 {
+                   
                     for (int k = 0; k < adjacencyMatrix.GetLength(1); k++)
                     {
                         System.Console.Write(adjacencyMatrix[i,k]+"  ");
@@ -53,14 +66,16 @@ namespace BinaryTreeImplementation
         {
 
 
-            List<string>vertices=new List<string>()
-            {
-              "A","B","C" 
-            };
+            List<string>vertices=new List<string>(){"A","B","C","D","E"};
 
             Graph graph=new Graph(vertices,Graph.GraphDirectionType.enDirected);
             
             graph.AddEdge("A","B",1);
+            graph.AddEdge("A","C",1);
+            graph.AddEdge("B","D",1);
+            graph.AddEdge("C","D",1);
+            graph.AddEdge("B","E",1);
+            graph.AddEdge("D","E",1);
 
 
             graph.DisplayMatix();
