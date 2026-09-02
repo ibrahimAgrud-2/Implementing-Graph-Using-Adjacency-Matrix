f50af25cd77473b99bd5d3f5a6cb099bbdc17a1f
ibrahim <ibrahimagrud7@gmail.com>
2026-09-02 11:10:42 +0300

    Display Matrix

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,16 +10,62 @@ namespace BinaryTreeImplementation
         public class Graph
         {
             
+            public enum GraphDirectionType {enDirected=1,eUndirected=2}; 
+
+            public GraphDirectionType enGraphDirectionType;
+
+
+            int[,] adjacencyMatrix;
+            List<string> Vertices;
+            public Graph(List<string> vertices,GraphDirectionType enGraphDirectionType)
+            {
+                adjacencyMatrix=new int[vertices.Count,vertices.Count];
+                this.Vertices=new List<string>(vertices);
+            }
+
+            public void AddEdge(string node1,string node2,int connectionValue)
+            {
+                if(Vertices.Contains(node1)&&Vertices.Contains(node2))
+                {
+                     adjacencyMatrix[Vertices.IndexOf(node1),Vertices.IndexOf(node2)]=connectionValue;
+                }
+            } 
+
+
+
+            public void DisplayMatix()
+            {
+               for (int i = 0; i < adjacencyMatrix.GetLength(0); i++)
+                {
+                    for (int k = 0; k < adjacencyMatrix.GetLength(1); k++)
+                    {
+                        System.Console.Write(adjacencyMatrix[i,k]+"  ");
+                    }
+                    System.Console.WriteLine();
+                }
+            }
+
+
         }
 
 
         static void Main(string[] args)
-        {Console.Clear();
+        {
 
-            System.Console.WriteLine("");
-            System.Console.WriteLine("");
-            System.Console.WriteLine("");
- 
+
+            List<string>vertices=new List<string>()
+            {
+              "A","B","C" 
+            };
+
+            Graph graph=new Graph(vertices,Graph.GraphDirectionType.enDirected);
+            
+            graph.AddEdge("A","B",1);
+
+
+            graph.DisplayMatix();
+         
+
         }
     }
 }
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d5c91736ec9220896d5bb3c4d78d8c599c72eae")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
